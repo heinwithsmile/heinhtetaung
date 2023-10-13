7f7314a162a63831857b4d519ba10a4266ad8981
otech-hein <otechhein27923@gmail.com>
2023-10-13 14:56:23 +0630

    Before the change theme

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,7 +1,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
-    console.log('Hello World')
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
     }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,53 @@
-const jobTitles = ["Web Developer", "IT Trainer", "Freelancer"];
-let currentIndex = 0;
+// const jobTitles = ["Web Developer", "IT Trainer", "Freelancer"];
+// let currentIndex = 0;
 
-function updateJobText() {
-  const jobElement = document.getElementById("career");
-  if (jobElement) {
-    jobElement.textContent = jobTitles[currentIndex];
-    currentIndex = (currentIndex + 1) % jobTitles.length;
-  } else {
-    console.error("Element with id 'career' not found.");
+// function updateJobText() {
+//   const jobElement = document.getElementById("career");
+//   if (jobElement) {
+//     jobElement.textContent = jobTitles[currentIndex];
+//     currentIndex = (currentIndex + 1) % jobTitles.length;
+//   } else {
+//     console.error("Element with id 'career' not found.");
+//   }
+// }
+
+// updateJobText();
+// setInterval(updateJobText, 3000);
+
+/* Smooth Effect for Links */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+/* Scroll to Top Behavior */
+const btn = document.querySelector('.btn-top');
+
+btn.addEventListener('click', function() {
+  scroll(0, 500);
+})
+
+window.onscroll = function showHide() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    btn.style.transform = 'scale(1)';
+  }
+  else {
+    btn.style.transform = 'scale(0)';
   }
 }
 
-updateJobText();
-setInterval(updateJobText, 3000);
+function scroll (target, duration) {
+  if (duration <= 0) {return};
+  let difference = target - document.documentElement.scrollTop;
+  let speed = difference / duration * 10;
+  setTimeout(function() {
+    document.documentElement.scrollTop += speed;
+    if (document.documentElement.scrollTop == target) {return};
+    scroll(target, duration - 10);
+  }, 10);
+}
